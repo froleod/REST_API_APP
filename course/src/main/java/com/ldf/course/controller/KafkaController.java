578e8ca0056e588ef098623aaec05f3319b4b1d0
Leonid Frolov <froleod@gmail.com>
2024-07-07 18:06:00 +0300

    added kafka

---
+++ course/src/main/java/com/ldf/course/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.ldf.course.controller;
+
+import com.ldf.course.kafka.KafkaProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public KafkaController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @PostMapping("/send")
+    public String send(@RequestBody String message) {
+        kafkaProducer.send(message);
+        return "success!";
+    }
+}
---
+++ course/src/main/java/com/ldf/course/kafka/KafkaProducer.java
@@ -0,0 +1,17 @@
+package com.ldf.course.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String message) {
+        kafkaTemplate.send("kafkaTest1", message);
+    }
+}
